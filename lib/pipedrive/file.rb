c984d70f24c3fce85926b5486fbde5ab678d8f65
Xiao Xie <xiao@kamiapp.com>
2023-07-27 13:57:33 +1200

    Add support to add remote file/file link

--- lib/pipedrive/file.rb
+++ lib/pipedrive/file.rb
@@ -6,5 +6,13 @@ module Pipedrive
     include ::Pipedrive::Operations::Read
     include ::Pipedrive::Operations::Update
     include ::Pipedrive::Operations::Delete
+
+    def create_remote(params)
+      make_api_call(:post, :remote, params)
+    end
+
+    def create_remote_link(params)
+      make_api_call(:post, :remoteLink, params)
+    end
   end
 end
